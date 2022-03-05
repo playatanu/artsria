a510b7627163e29119bde8d2005f04effb2f82b6
playatanu <playatanu@gmail.com>
2022-03-06 00:35:42 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,38 @@
-import styles from '../styles/Home.module.css'
-import axios from 'axios'
+import styles from '../styles/Home.module.css';
+import axios from 'axios';
 
-import {uploadImage}  from "../lib/services/uploadImage";
+import { uploadImage } from '../lib/services/uploadImage';
 
-import {useState} from 'react'
+import { useState } from 'react';
 
 export default function Home() {
-  const [name, setName] = useState()
-	const [file, setFile] = useState()
-	
-    const run = async() => {
-      try {
-    const res = await axios.get('https://api.playatanu.repl.co/image/')
+  const [name, setName] = useState();
+  const [file, setFile] = useState();
 
-        setName(res.data.server)
-      }
+  const run = async () => {
+    try {
+      const res = await axios.get('https://api.playatanu.repl.co/image/');
 
-      catch(err) {
+      setName(res.data.server);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-        console.log(err);
-      }
-      
-  }
+  const handleFileSelect = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const uploadfile = async () => {
+    const res = await uploadImage(file);
 
-	const handleFileSelect = (event) => {
-    setFile(event.target.files[0])
-  }
-   const uploadfile = async() =>{
+    setName(res);
+  };
 
-		const res = await uploadImage(file);
-
-		setName(res);
-	
-		 
- }
-	
   return (
-    <div className={styles.container} onClick={()=>run()}>
-     Hello {name}
-    <input onChange={handleFileSelect} type="file"/>
-			<button onClick={uploadfile}>upload</button>
+    <div className={styles.container} onClick={() => run()}>
+      Hello {name}
+      <input onChange={handleFileSelect} type="file" />
+      <button onClick={uploadfile}>uploadq</button>
     </div>
-  )
+  );
 }
