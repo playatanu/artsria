6c32f7f85f9cbbefd91643c0aa47f68940017a7d
Atanu Debnath <70248732+playatanu@users.noreply.github.com>
2022-03-05 19:01:49 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import styles from '../styles/Home.module.css'
+import axios from 'axios'
+
+import {uploadImage}  from "../lib/services/uploadImage";
+
+import {useState} from 'react'
+
+export default function Home() {
+  const [name, setName] = useState()
+	const [file, setFile] = useState()
+	
+    const run = async() => {
+      try {
+    const res = await axios.get('https://api.playatanu.repl.co/image/')
+
+        setName(res.data.server)
+      }
+
+      catch(err) {
+
+        console.log(err);
+      }
+      
+  }
+
+	const handleFileSelect = (event) => {
+    setFile(event.target.files[0])
+  }
+   const uploadfile = async() =>{
+
+		const res = await uploadImage(file);
+
+		setName(res);
+	
+		 
+ }
+	
+  return (
+    <div className={styles.container} onClick={()=>run()}>
+     Hello {name}
+    <input onChange={handleFileSelect} type="file"/>
+			<button onClick={uploadfile}>upload</button>
+    </div>
+  )
+}
